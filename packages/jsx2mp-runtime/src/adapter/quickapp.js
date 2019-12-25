1eb4db11f5177aa785402e260f9716049988f6d6
小滐 <wlj136209@alibaba-inc.com>
2019-12-25 15:56:52 +0800

    fix: didmount多次执行的bug

--- packages/jsx2mp-runtime/src/adapter/quickapp.js
+++ packages/jsx2mp-runtime/src/adapter/quickapp.js
@@ -61,18 +61,67 @@ export function attachEvent(isPage, config, proxiedMethods) {
 }
 
 export function updateData(data) {
-  Object.keys(data).map(item => {
-    if (!(item in this._internal)) {
-      this._internal.$set(item, data[item])
+  let $ready = false;
+  const useSetData = {};
+
+  for (let key in data) {
+    if (Array.isArray(data[key]) && diffArray(this.state[key], data[key])) {
+      useSetData[key] = [this.state[key].length, 0].concat(data[key].slice(this.state[key].length));
     } else {
-      this._internal[item] = data[item]
+      if (diffData(this.state[key], data[key])) {
+        useSetData[key] = data[key];
+      }
+    }
+  }
+
+  if (!isEmptyObj(useSetData)) {
+    $ready = useSetData.$ready;
+  }
+
+  Object.keys(useSetData).map(item => {
+    if (!(item in this._internal)) {
+      this._internal.$set(item, useSetData[item])
+    } else {
+      this._internal[item] = useSetData[item]
     }
   });
 
-  // trigger did mount
-  this._trigger(COMPONENT_DID_MOUNT);
+  if ($ready) {
+    // trigger did mount
+    this._trigger(COMPONENT_DID_MOUNT);
+  }
   let callback;
   while (callback = this._pendingCallbacks.pop()) {
     callback();
+  }
+}
+
+function isEmptyObj(obj) {
+  for (let key in obj) {
+    return false;
+  }
+  return true;
+}
+
+function diffArray(prev, next) {
+  if (!Array.isArray(prev)) return false;
+  // Only concern about list append case
+  if (next.length === 0) return false;
+  if (prev.length === 0) return false;
+  return next.slice(0, prev.length).every((val, index) => prev[index] === val);
+}
+
+function diffData(prevData, nextData) {
+  const prevType = typeof prevData;
+  const nextType = typeof nextData;
+  if (prevType !== nextType) return true;
+  if (prevType === 'object' && prevData !== null && nextData !== null) {
+    const prevKeys = Object.keys(prevData);
+    const nextKeys = Object.keys(nextData);
+    if (prevKeys.length !== nextKeys.length) return true;
+    if (prevKeys.length === 0) return false;
+    return !prevKeys.every(key => prevData[key] === nextData[key] );
+  } else {
+    return prevData !== nextData;
   }
 }
